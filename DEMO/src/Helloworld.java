faf8467bde23ffe5767c8c76cb516410deb18167
hf0353je <hf0353je@W3110-S1664730.century.edu>
2019-10-14 13:18:16 -0500

    added

--- DEMO/src/Helloworld.java
+++ DEMO/src/Helloworld.java
@@ -1,4 +1,12 @@
 
 public class Helloworld {
+	
+	static int speed = 323;
+	static String car = "Honda";
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Car:" + car + "Speed: " + speed +  "mph");
+	}
 
 }
